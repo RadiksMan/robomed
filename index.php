03f7f352c4b4ce23c62dc7dfa7bf4851b49f16f2
Radiksman <radion91@gmail.com>
2015-11-02 17:32:14 +0200

    ad  file

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
-			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
+		<?partial('slider')?>
+		<?partial('text-top')?>
+		<?partial('big-sircle')?>
 	</div>
