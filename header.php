bd81b711582134bdd26954ee8d411ccdfb530a3d
Radiksman <radion91@gmail.com>
2015-11-02 17:08:12 +0200

    add basic

---
+++ header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+	<link rel="shortcut icon" type="image/x-icon" href="images/shipfish.ico">
+
+	<title></title>
+
+	<!--<link rel="stylesheet" href="css/bootstrap.css" media="all" />
+	<link rel="stylesheet" href="css/bootstrap-theme.css" media="all" />-->
+
+	<link rel="stylesheet" type="text/css" href="css/style_basic.css" media="all" />
+
+	<link rel="stylesheet" type="text/css" href="css/fonts.css" media="all" />
+
+
+
+	<!--<link rel="stylesheet" href="css/tuesdayAnimate.css" media='all' />-->
+
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+
+	<!-- bootstrap -->
+
+	<!--<script src="js/bootstrap.js" type="text/javascript"></script>-->
+
+	<!-- /bootstrap -->
+
+	<!-- form styler -->
+
+	<link rel="stylesheet" href="css/jquery.formstyler.css" media='all' />
+	<script src="js/jquery.formstyler.js" type="text/javascript"></script>
+
+	<!-- /form styler -->
+
+	<script src="js/selectivizr-min.js" type="text/javascript"></script>
+
+	<script src="js/jquery.validate.min.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox.css" media="all" />
+	<script src="fancybox/jquery.fancybox.js" type="text/javascript"></script>
+
+	<script src="js/modernizr.js" type="text/javascript"></script>
+	<script src="js/jquery.watermark.min.js" type="text/javascript"></script>
+
+<!-- mask for inputs -->
+
+	<script src="js/maskInput.js" type="text/javascript"></script>
+
+<!-- /mask for inputs -->
+
+<!-- sliders -->
+	<link rel="stylesheet" type="text/css" href="css/slick.css" media="all" />
+	<script src="js/slick.js" type="text/javascript"></script>
+<!-- sliders -->
+
+<!-- jScrollPane -->
+	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+<!-- jScrollPane -->
+
+<!-- google Maps -->
+
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script>
+		var cordX = 55.822389;
+		var cordY= 37.647088;
+		var googleText = "Москва, Проспект Мира д. 150";
+	</script>
+	<script src='js/googleMapsScr.js'></script>
+
+
+<!-- /google Maps -->
+
+<!-- remove Files -->
+
+	<script src="js/radion.js" type="text/javascript"></script>
+
+	<script src="js/artem.js" type="text/javascript"></script>
+
+	<script src="js/roma.js" type="text/javascript"></script>
+
+	<script src="js/nikolay.js" type="text/javascript"></script>
+
+	<!--<script src="js/myalert.js" type="text/javascript"></script>
+	<script src="develop/autocssrenew.js" type="text/javascript"></script>-->
+
+<!-- remove Files -->
+
+	<link rel="stylesheet" type="text/css" href="css/artem.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/nikolay.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/roma.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/radion.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/style_main.css" media="all" />
+
+	<link rel="stylesheet" type="text/css" href="css/adaptation_artem.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/adaptation_nikolay.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/adaptation_roma.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/adaptation_radion.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/adaptation.css" media="all" />
+
+	<script src="js/scr.js" type="text/javascript"></script>
+
+	<!--[if lt IE 9]>
+		<script>
+		document.createElement('header');
+		document.createElement('nav');
+		document.createElement('section');
+		document.createElement('article');
+		document.createElement('aside');
+		document.createElement('footer');
+		document.createElement('time');
+		</script>
+		<script src="js/pie.js" type="text/javascript"></script>
+	<![endif]-->
+
+	<!--[if lt IE 8]><script src="js/oldie/warning.js"></script><script>window.onload=function(){e("js/oldie/")}</script><![endif]-->
+</head>
+<body>
+	<?partial('zHiddenBlock');?>
+	<header class="header">
+		<div class="mbox">
+
+		</div>
+	</header>
---
+++ js/scr.js
@@ -0,0 +1,262 @@
+var scroller=jQuery.browser.webkit ? "body": "html";
+
+/* modernize */
+function modernize() {
+	// placeholder
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').each(function() {
+			$(this).watermark($(this).attr('placeholder'));
+		});
+	}
+}
+
+
+/* input only Number  */
+function inputNumber(block) {
+	$('input', block).keypress(function(e) {
+		if (e.which >= 47 && e.which <= 57 ){}
+		else return false;
+	});
+
+	$('input', block).keyup(function() {
+		$inputNum = $(this);
+		if ($inputNum.val == '' || $inputNum.val() == 0) {
+			$inputNum.val('');
+		}
+	});
+}
+
+
+/* u_tabs */
+function u_tabs(link, block) {
+	$(link).click(function(e) {
+		var $currentTab = $(this);
+		var tabId = $currentTab.data('utab');
+
+		$(link).removeClass('active');
+		$currentTab.addClass('active');
+
+		$(block).hide().removeClass('active');
+		$(block+'[data-utab="' + tabId + '"]').show().addClass('active');
+		if($(link).is('a')){
+			e.preventDefault();
+		}
+	});
+	$(link).eq(0).click();
+}
+
+/* scrollUp */
+function scrollUp(block,targetBlock) {
+
+	$(block).click(function(e){
+		var target = $(targetBlock).offset().top;
+
+		$(scroller).animate({scrollTop:target},800);
+		return false;
+
+		e.preventDefault();
+	});
+}
+
+
+function initScrollpane(){
+	$('.scroll-pane').jScrollPane();
+}
+
+
+function oneHeightItems(){
+
+	function oneHeight(block){
+		var height=0;
+		block.removeAttr('style');
+		block.each(function(){
+			if($(this).height()>height){
+				height=$(this).height();
+			}
+		});
+		block.css('height', height);
+	}
+
+	oneHeight($('.oneHeight'));
+}
+
+function validate(form, options){
+    var setings = {
+        errorFunction:null,
+        submitFunction:null,
+        highlightFunction:null,
+        unhighlightFunction:null
+    }
+    $.extend(setings, options);
+
+    var $form = $(form);
+
+    if ($form.length && $form.attr('novalidate') === undefined) {
+        $form.on('submit', function(e) {
+            e.preventDefault();
+        });
+
+        $form.validate({
+            errorClass : 'errorText',
+            focusCleanup : true,
+            focusInvalid : false,
+            invalidHandler: function(event, validator) {
+                if(typeof(setings.errorFunction) === 'function'){
+                    setings.errorFunction(form);
+                }
+            },
+            errorPlacement: function(error, element) {
+                error.appendTo( element.closest('.form_input'));
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('error');
+                $(element).closest('.form_row').addClass('error').removeClass('valid');
+                if( typeof(setings.highlightFunction) === 'function' ) {
+                    setings.highlightFunction(form);
+                }
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('error');
+                if($(element).closest('.form_row').is('.error')){
+                    $(element).closest('.form_row').removeClass('error').addClass('valid');
+                }
+                if( typeof(setings.unhighlightFunction) === 'function' ) {
+                    setings.unhighlightFunction(form);
+                }
+            },
+            submitHandler: function(form) {
+                if( typeof(setings.submitFunction) === 'function' ) {
+                    setings.submitFunction(form);
+                } else {
+                    form.submit();
+                }
+            }
+        });
+
+        $('[required]',$form).each(function(){
+            $(this).rules( "add", {
+                required: true,
+                messages: {
+                    required: "Вы пропустили"
+                }
+            });
+        });
+
+        if($('[type="email"]',$form).length) {
+            $('[type="email"]',$form).rules( "add",
+            {
+                messages: {
+                    email: "Невалидный email"
+                 }
+            });
+        }
+
+        if($('.tel-mask[required]',$form).length){
+            $('.tel-mask[required]',$form).rules("add",
+            {
+                messages:{
+                    required:"Введите номер мобильного телефона."
+                }
+            });
+        }
+    }
+}
+
+function validationCall(form){
+
+  var thisForm = $(form);
+  var formSur = thisForm.serialize();
+
+    $.ajax({
+        url : thisForm.attr('action'),
+        data: formSur,
+        method:'POST',
+        success : function(data){
+            if ( data.trim()!='true') {
+                thisForm.trigger("reset");
+                popNext();
+            }
+            else {
+               $(this).trigger('reset');
+            }
+
+        }
+    });
+
+    function popNext(){
+        $.fancybox.open("#call_success",{
+            padding:0,
+            fitToView:false,
+            wrapCSS:"call-popup",
+            autoSize:true,
+            afterClose: function(){
+                $('form').trigger("reset");
+                clearTimeout(timer);
+            }
+        });
+        var timer = null;
+
+        timer = setTimeout(function(){
+            $('form').trigger("reset");
+            $.fancybox.close("#call_success");
+        },2000);
+
+
+    }
+}
+
+function animationBlock(item){
+
+    $(window).scroll(function(){
+        checkForAnimate();
+    });
+
+    function checkForAnimate(){
+        var bottomCheck = $(window).height()+$(window).scrollTop();
+        var windowTop = $(window).scrollTop()+($(window).height()/1.5);
+        item.each(function(){
+           if(windowTop>$(this).offset().top || bottomCheck > $('body').height()*0.98){
+
+              var itemSect = $(this);
+              var point = 0;
+              itemSect.find('.animate-it').addClass('animated');
+
+              var timer = setInterval(function(){
+                 itemSect.find('.animate-delay').eq(point).addClass('animated');
+                 point++;
+                 if(itemSect.find('.animate-delay').length == point){
+                     clearInterval(timer);
+                 }
+              },200);
+
+
+           }
+        });
+    }
+    checkForAnimate();
+}
+
+/* DOCUMENT READY  */
+$(document).ready(function() {
+	modernize();
+	$('.footer_placeholder').height($('.footer').outerHeight());
+
+	oneHeightItems();
+});
+
+$(window).load(function(){
+
+
+
+});
+
+$(window).resize(function() {
+
+    $('.footer_placeholder').height($('.footer').outerHeight());
+});
+
+
+
+
+
+
