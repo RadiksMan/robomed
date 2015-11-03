c23f8f695cec1d089df76867da478bf94a8a509c
Radiksman <radion91@gmail.com>
2015-11-03 09:40:00 +0200

    add slider 40%

---
+++ partials/slider.php
@@ -0,0 +1,12 @@
+<div class="main_slider">
+    <div class="main_slider_wrap">
+        <div class="slider_row">
+            <div class="slider_item">
+                <img src="images/slider_img_1.jpg" alt="">
+            </div>
+            <div class="slider_item">
+                <img src="images/slider_img_1.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
