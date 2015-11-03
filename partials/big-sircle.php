f89a9705ac272d791e16deeca6ed398361e072e4
Dmitry <dmitry1940@mail.ru>
2015-11-03 10:49:23 +0200

    added markup to big circle

--- partials/big-sircle.php
+++ partials/big-sircle.php
@@ -1,43 +1,153 @@
 <div class="big-sircle">
     <div class="big-circle-wrap">
-        <div class="big-sircle-part">
-            <div class="big-sircle-list">
-                <ul>
-                    <li></li>
-                </ul>
-            </div>
-            <div class="big-sircle-part-wrap">
-                <div class="sircle-part-left">
-                    <div class="big-sircle-part-item">
-                        <img src="images/circle-part-left.png" alt="" />
+        <div class="big-sircle-wrap-outside">
+            <div class="big-circle-wrap-inside">
+                <div class="big-sircle-part">
+                    <div class="big-sircle-list">
+                        <ul>
+                            <li>
+                                <span class="list-text">Склад</span>
+                                <span class="list-num">01</span>
+                            </li>
+                            <li>
+                                <span class="list-text">Расчет зарплаты по KPI</span>
+                                <span class="list-num">02</span>
+                            </li>
+                            <li>
+                                <span class="list-text">Билинг</span>
+                                <span class="list-num">03</span>
+                            </li>
+                            <li>
+                                <span class="list-text">Расходники</span>
+                                <span class="list-num">04</span>
+                            </li>
+                            <li>
+                                <span class="list-text">Визуализация результатов для персонала</span>
+                                <span class="list-num">05</span>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="big-sicrcle-part-item-active">
-                        <img src="images/circle-part-left-active.png" alt="" />
-                    </div>
-                    <div class="big-sircle-text left-text">
-                        <p>ERP</p>
-                        <p>система</p>
-                        <p>учета</p>
+                    <div class="big-sircle-part-wrap">
+                        <div class="sircle-part-left">
+                            <div class="big-sircle-part-item">
+                                <img src="images/circle-part-left-active.png" alt="" />
+                            </div>
+                            <div class="big-sicrcle-part-item-active">
+                                <img src="images/circle-part-left-deactive.png" alt="" />
+                            </div>
+                            <div class="big-sircle-text left-text">
+                                <p><span>ERP</span></p>
+                                <p>система</p>
+                                <p>учета</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="big-sircle-part-wrap">
-                <div class="sircle-part-top">
-                    <div class="big-sircle-part-item">
-                        <img src="images/circle-part-top.png" alt="" />
+                <div class="big-sircle-part">
+                    <div class="big-sircle-list">
+                        <ul>
+                            <li>
+                                <span class="list-num">01</span>
+                                <span class="list-text">Sms, e-mail рассылки</span>
+                            </li>
+                            <li>
+                                <span class="list-num">02</span>
+                                <span class="list-text">Лояльность</span>
+                            </li>
+                            <li>
+                                <span class="list-num">03</span>
+                                <span class="list-text">Запись, оплата</span>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="big-sicrcle-part-item-active">
-                        <img src="images/circle-part-top-active.png" alt="" />
+                    <div class="big-sircle-part-wrap">
+                        <div class="sircle-part-top">
+                            <div class="big-sircle-part-item">
+                                <img src="images/circle-part-top-active.png" alt="" />
+                            </div>
+                            <div class="big-sicrcle-part-item-active">
+                                <img src="images/circle-part-top-deactive.png" alt="" />
+                            </div>
+                            <div class="big-sircle-text top-text">
+                                <p><span>Личный</span></p>
+                                <p><span>кабинет</span></p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="big-sircle-text left-text">
-                        <p>Личный</p>
-                        <p>кабинет</p>
+                </div>
+                <div class="big-sircle-part">
+                    <div class="big-sircle-list">
+                        <ul>
+                            <li>
+                                <span class="list-num">01</span>
+                                <span class="list-text">Справочники</span>
+                            </li>
+                            <li>
+                                <span class="list-num">02</span>
+                                <span class="list-text">Карты пациентов</span>
+                            </li>
+                            <li>
+                                <span class="list-num">03</span>
+                                <span class="list-text">ДСМ</span>
+                            </li>
+                            <li>
+                                <span class="list-num">04</span>
+                                <span class="list-text">База знаний</span>
+                            </li>
+                            <li>
+                                <span class="list-num">05</span>
+                                <span class="list-text">Интерактивные шаблоны приемов</span>
+                            </li>
+                            <li>
+                                <span class="list-num">06</span>
+                                <span class="list-text">Dicom медицинское оборудование</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="big-sircle-part-wrap">
+                        <div class="sircle-part-right">
+                            <div class="big-sircle-part-item">
+                                <img src="images/circle-part-right-active.png" alt="" />
+                            </div>
+                            <div class="big-sicrcle-part-item-active">
+                                <img src="images/circle-part-right-deactive.png" alt="" />
+                            </div>
+                            <div class="big-sircle-text right-text">
+                                <p><span>EHR</span></p>
+                                <p>медицинская</p>
+                                <p>система</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="big-circle-part">
+                    <div class="big-sircle-list">
+                        <ul>
+                            <li>
+                                <span class="list-num">01</span>
+                                <span class="list-text">Конструктор цепочек бизнес процессов</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="big-sircle-part-wrap">
+                        <div class="sircle-part-bottom">
+                            <div class="big-sircle-part-item">
+                                <img src="images/circle-part-bottom-active.png" alt="" />
+                            </div>
+                            <div class="big-sicrcle-part-item-active">
+                                <img src="images/circle-part-bottom-deactive.png" alt="" />
+                            </div>
+                            <div class="big-sircle-text bottom-text">
+                                <p><span>Интерфейс</span></p>
+                                <p><span>пользователя</span></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="big-sircle-part"></div>
-        <div class="big-sircle-part"></div>
-        <div class="big-sircle-part"></div>
+        <div class="big-circle-wrap-center">
+            <img src="images/big-circle-center.png" alt="" />
+        </div>
     </div>
 </div>
