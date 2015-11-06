edfebb6e58d66063f703bd6357d6537a131564cc
Dmitry <dmitry1940@mail.ru>
2015-11-06 17:39:57 +0200

    fix for notpad

--- header.php
+++ header.php
@@ -4,7 +4,10 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta id="viewport" name="viewport" content="width=1280,user-scalable=no">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="MobileOptimized" content="1280"/>
+	<meta name="HandheldFriendly" content="false"/>
 
 	<link rel="shortcut icon" type="image/x-icon" href="images/shipfish.ico">
 
