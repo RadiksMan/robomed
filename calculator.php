b7684e4ad910e27011af533e15585ba876c1c9da
Dmitry <dmitry1940@mail.ru>
2015-11-18 16:39:30 +0200

    calculator markup

--- calculator.php
+++ calculator.php
@@ -28,4 +28,53 @@
 
     </div>
 
+    <div class="calculator">
+        <div class="calculator-wrap">
+            <form>
+                <div class="calculator-title">Введите текущие показатели:</div>
+                <div class="calculator-body">
+                    <div class="calculator-col">
+                        <div class="calculator-col-title">Пациентов в месяц:</div>
+                        <div class="calculator-input">
+                            <input type="text" name="patients" />
+                        </div>
+                    </div>
+                    <div class="calculator-col">
+                        <div class="calculator-col-title">ФОТ:</div>
+                        <div class="calculator-input">
+                            <input type="text" name="fot" />
+                        </div>
+                    </div>
+                    <div class="calculator-col">
+                        <div class="calculator-col-title">Средний чек (руб):</div>
+                        <div class="calculator-input">
+                            <input type="text" name="check" />
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="calculator-wrap-bottom color-line">
+            <div class="calculator-optimization">
+                <div class="calculator-optimization-top">
+                    <div class="calculator-optimization-col optimization-first-col">
+                        <div class="optimiztion-value"></div>
+                        <div class="optimization-text">Оптимизация рекламы по аналитике</div>
+                    </div>
+                    <div class="calculator-optimization-col optimization-second-col">
+                        <div class="optimiztion-value"></div>
+                        <div class="optimization-text">За счет сокращения неэффективных сотрудников</div>
+                    </div>
+                    <div class="calculator-optimization-col optimization-third-col">
+                        <div class="optimiztion-value"></div>
+                        <div class="optimization-text">За счет продаж комплексов услуг</div>
+                    </div>
+                </div>
+                <div class="calculator-optimization-bottom">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
