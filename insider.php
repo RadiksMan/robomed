f70e015a6369763619d3c71426d464ec02dafe14
Dmitry <dmitry1940@mail.ru>
2015-11-18 15:14:46 +0200

    added insider page

---
+++ insider.php
@@ -0,0 +1,17 @@
+<div class="main">
+
+    <div class="title-wrap">
+        <div class="mbox">
+            <div class="breadcrumbs">
+                <span>
+                    <a href="#">Главная</a>
+                </span>
+                <span>Внедрение</span>
+            </div>
+            <div class="title-main">
+                внедрение
+            </div>
+        </div>
+    </div>
+
+</div>
