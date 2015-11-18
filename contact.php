e55a665531bd45bb2b33d50de45b6859bd3a314a
Radiksman <radion91@gmail.com>
2015-11-18 17:36:51 +0200

    add contact 70%

---
+++ contact.php
@@ -0,0 +1,80 @@
+<div class="contact">
+    <div class="contact-wrap">
+        <div class="contact-google-map">
+            <div id="google-map"></div>
+        </div>
+        <div class="contact-wrap-text">
+            <div class="contact-text">
+                <div class="mbox">
+                    <div class="contact-top">
+                        <div class="contact-top-text">
+                            <div class="contact-top-text-desc">
+                                <p>г. Санкт-Петербург</p>
+                                <p>123344, Рыбный переулок д. 5</p>
+                            </div>
+                            <div class="contact-top-text-tel">
+                                <span class="icon icon_tel"></span>
+                                <a href="tel:88008621223">8 800 862-12-23</a>
+                            </div>
+                            <div class="contact-top-text-mail">
+                                <span class="icon icon_mail"></span>
+                                <a href="mail:robomed@mail.ru">robomed@mail.ru</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contact-bottom">
+                        <div class="contact-bottom-form">
+                            <form action="" name="">
+                                <div class="contact-form-title">
+                                    <h5>Остались вопросы? заполните заявку и мы свяжемся с вами</h5>
+                                </div>
+                                <div class="contact-form-row cfix">
+                                    <div class="contact-form-item">
+                                        <div class="contact-form-item-text">
+                                            <span>Ваше имя<span>*</span>:</span>
+                                        </div>
+                                        <div class="contact-form-item-input">
+                                            <input type="text" name="contact_name" required="required" value="Андрей">
+                                        </div>
+                                    </div>
+                                    <div class="contact-form-item">
+                                        <div class="contact-form-item-text">
+                                            <span>Ваш e-mail<span>*</span>:</span>
+                                        </div>
+                                        <div class="contact-form-item-input">
+                                            <input type="text" name="contact_name" required="required" value="mail@mail.ru">
+                                        </div>
+                                    </div>
+                                    <div class="contact-form-item">
+                                        <div class="contact-form-item-text">
+                                            <span>Ваш телефон:</span>
+                                        </div>
+                                        <div class="contact-form-item-input">
+                                            <input type="text" name="contact_name" value="+7 (123) 456-77-88">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="contact-form-row cfix">
+                                    <div class="contact-form-item-sur">
+                                        <div class="contact-form-item-text">
+                                            <span>Дополнительная информация:</span>
+                                        </div>
+                                        <div class="contact-form-item-input">
+                                            <textarea name="" id="" cols="10" rows="1">Ваш вопрос</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="contact-form-item">
+                                        <button type="submit" class="contact-submit">
+                                            <span>Отправть</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-scrolling"></div>
+        </div>
+    </div>
+</div>
--- js/develop_4.js
+++ js/develop_4.js
@@ -9,7 +9,41 @@ function mailSlider(){
         arrows:false
     });
 }
+function googleMap(mapWrap){
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(cordX,cordY);
+        var myOptions = {
+            zoom: 16,
+            center: myLatlng,
+            disableDefaultUI: false, //без управляющих елементов
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
+            zoomControlOptions: {
+               position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
+            }
+        }
+        var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
+
+        var contentString = '<div class="marker-test">'+googleText+'</div>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        var image = 'images/marker.png';   // иконка картинкой
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            animation: google.maps.Animation.DROP, // анимация при загрузке карты
+            icon: image //  иконка картинкой
+        });
+
+
+
+    }
+    initialize();
+}
 
 $(document).ready(function() {
     mailSlider();
+    googleMap('google-map'); //id
 });
