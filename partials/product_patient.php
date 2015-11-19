765bd7ce2120c4d702d579e0b5146ee06a2c7ffa
ArtGarm <artem.garmatyk@gmail.com>
2015-11-19 14:43:14 +0200

    fd

---
+++ partials/product_patient.php
@@ -0,0 +1,21 @@
+<div class="patient-wrap">
+    <div class="patient-box">
+        <figure class="image">
+            <div class="image-conteiner">
+                <img src="images/happypatient.png" alt="">
+            </div>
+            <figurecaption>
+                <div class="list-number"><span>3</span> /3</div>
+                <div class="patient-header"> Для пациента </div>
+                <div class="text-about"> Системно увеличивается качество лечения, за счет внедрения стандартов и фалгоритмов для врачей </div>
+                <div class="list-for-standart">
+                    <ul>
+                        <li> Стандартизированные схемы лечения –  это уверенность во всех предоставляемых медицинских услугах.</li>
+                        <li> Личный кабинет – это удобная запись и оплата без очередей и регистратур.</li>
+                        <li> Sms, e-mail рассылки – напоминания о предстоящем приеме, результаты лечения и многое другое в Вашем мобильном телефоне.</li>
+                    </ul>
+                </div>
+            </figurecaption>
+        </figure>
+    </div>
+</div>
