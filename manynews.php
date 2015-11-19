4cd26ae2ed838928443558694e889fc1d364372c
Radiksman <radion91@gmail.com>
2015-11-19 16:47:09 +0200

    add button spesheli for artem

---
+++ manynews.php
@@ -0,0 +1,22 @@
+<div class="manynews">
+    <div class="manynews-title">
+        <div class="mbox">
+            <div class="contact-title">
+                <div class="breadcrumbs">
+                    <span>
+                        <a href="#">Главная</a>
+                    </span>
+                    <span>Новости</span>
+                </div>
+                <div class="title-main">
+                    Новости
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mbox">
+        <div class="manynews-wrap">
+
+        </div>
+    </div>
+</div>
