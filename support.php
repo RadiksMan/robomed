9b3c1b3624e62c6cee66bdbe824756f9095fb5e9
Dmitry <dmitry1940@mail.ru>
2015-11-19 11:36:47 +0200

    add support

---
+++ support.php
@@ -0,0 +1,21 @@
+<div class="support">
+    <div class="support-wrap">
+        <div class="support-title">
+            <div class="mbox">
+                <div class="breadcrumbs">
+                    <span>
+                        <a href="#">Главная</a>
+                    </span>
+                    <span>Поддержка</span>
+                </div>
+                <div class="title-main">
+                    Поддержка
+                </div>
+            </div>
+        </div>
+        <div class="support-middle cfix">
+            <div class="support-aside-main"></div>
+            <div class="support-aside-sub"></div>
+        </div>
+    </div>
+</div>
