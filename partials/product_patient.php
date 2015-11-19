f21135f5a409cb49f39e6a181ffbd1cc5da07cad
ArtGarm <artem.garmatyk@gmail.com>
2015-11-19 15:31:19 +0200

    xsdfvg

--- partials/product_patient.php
+++ partials/product_patient.php
@@ -1,21 +1,28 @@
-<div class="patient-wrap">
-    <div class="patient-box">
-        <figure class="image">
-            <div class="image-conteiner">
-                <img src="images/happypatient.png" alt="">
-            </div>
-            <figurecaption>
-                <div class="list-number"><span>3</span> /3</div>
-                <div class="patient-header"> Для пациента </div>
-                <div class="text-about"> Системно увеличивается качество лечения, за счет внедрения стандартов и фалгоритмов для врачей </div>
-                <div class="list-for-standart">
-                    <ul>
-                        <li> Стандартизированные схемы лечения –  это уверенность во всех предоставляемых медицинских услугах.</li>
-                        <li> Личный кабинет – это удобная запись и оплата без очередей и регистратур.</li>
-                        <li> Sms, e-mail рассылки – напоминания о предстоящем приеме, результаты лечения и многое другое в Вашем мобильном телефоне.</li>
-                    </ul>
+<div class="tabs-item-top">
+     <!-- artem marking  background-patient.png-->
+
+    <div class="patient-wrap">
+        <div class="patient-box mbox">
+            <figure class="patient-container">
+                <div class="image-conteiner">
+                    <img src="images/happypatient.png" alt="">
                 </div>
-            </figurecaption>
-        </figure>
+                <figurecaption class="subtext-conteiner">
+                    <div class="list-number"><span>3</span> /3</div>
+                    <div class="patient-header"> Для пациента </div>
+                    <div class="text-about"> Системно увеличивается качество лечения, за счет внедрения стандартов и фалгоритмов для врачей </div>
+                    <div class="list-for-standart">
+                        <ol>
+                            <li> Стандартизированные схемы лечения –  это уверенность во всех предоставляемых медицинских услугах.</li>
+                            <li> Личный кабинет – это удобная запись и оплата без очередей и регистратур.</li>
+                            <li> Sms, e-mail рассылки – напоминания о предстоящем приеме, результаты лечения и многое другое в Вашем мобильном телефоне.</li>
+                        </ol>
+                    </div>
+                </figurecaption>
+            </figure>
+        </div>
     </div>
+
+    <!-- dmitry marking -->
+
 </div>
