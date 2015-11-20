e64e072cd35ca25f5fef3bb3936f4cf7dc5f446c
trolizmaslom <trolizmaslom@gmail.com>
2015-11-20 18:01:41 +0200

    fix

--- intro.php
+++ intro.php
@@ -22,6 +22,70 @@
                 <p>необходимы для осуществления продаж и оказания услуг</p></span>
 
         </div>
+        <div class="circle-block">
+            <div class="circle-title"><span>ЭФФЕКТ ОТ ВНЕДРЕНИЯ ROBOMED:</span></div>
+            <div class="circle-items-wrapp jfix">
+                <div class="circl-item">
+                    <div class="item-img"><img src="images/intro01.png"  alt=""></div>
+                    <div class="item-title"><span>Вырастут рекомендации среди пациентов</span></div>
+                    <div class="title-text"><span>За счет улучшения качества лечения</span></div>
+                </div>
+                <div class="circl-item">
+                    <div class="item-img"><img src="images/intro02.png"  alt=""></div>
+                    <div class="item-title"><span>Снизятся расходы на рекламу</span></div>
+                    <div class="title-text"><span>За счет удешевления воронки продаж</span></div>
+                </div>
+                <div class="circl-item">
+                    <div class="item-img"><img src="images/intro03.png"  alt=""></div>
+                    <div class="item-title"><span>Вы точно будете знать показатели каждого врача</span></div>
 
+                </div>
+                <div class="circl-item">
+                    <div class="item-img"><img src="images/intro04.png"  alt=""></div>
+                    <div class="item-title"><span>Врачу не нужно будет«продавать» услуги</span></div>
+
+                </div>
+            </div>
+        </div>
+        <div class="map-block">
+            <div class="back"><img src="images/introduction-map.png" alt=""></div>
+            <div class="map-align cfix">
+                <div class="left-box">
+                    <div class="text-blocker">
+                        <span>
+                            <p>Система «Робомед» создана в России на основе международного и</p>
+                            <p> отечественного опыта. Главное отличие от существующих</p>
+                            <p> российских систем «Робомед» является основой для более эффективной</p>
+                            <p> бизнес-модели клиники.</p>
+                            <p> Она успешно протестирована в реалиях российских клиник </p>
+                            <p>и доказала свою эффективность.</p>
+                        </span>
+
+                    </div>
+                    <div class="bttn-box">
+                        <a href="#" class="bttn-text"><span>этапы внедрения МИС Robomed</span></a>
+                    </div>
+                </div>
+                <div class="right-box">
+                    <div class="red-text">
+                        <span>
+                            <p>Главное отличие</p>
+                            <p>от существующих</p>
+                            <p>российских систем</p>
+                        </span>
+                    </div>
+                    <div class="black-text">
+                        <span>
+                            <p>«Робомед» является</p>
+                            <p>основой</p>
+                            <p>для более</p>
+                            <p>эффективной</p>
+                            <p>бизнес-модели</p>
+                            <p>клиники</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
