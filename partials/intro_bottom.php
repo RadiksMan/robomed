5f42ea1104524cfff25e53711ca098495efddbad
Dmitry <dmitry1940@mail.ru>
2015-11-20 17:44:11 +0200

    intro bottom  added

---
+++ partials/intro_bottom.php
@@ -0,0 +1,32 @@
+<div class="intro-bottom color-line">
+    <div class="mbox">
+        <div class="intro-bottom-main cfix">
+            <div class="intro-bottom-col">
+                <div class="intro-bottom-top-text">Переход от эффективности отдельного врача</div>
+                <div class="intro-bottom-bottom-text">К внедрению стандартов медицинских знаний внутри клиники</div>
+            </div>
+            <div class="intro-bottom-col">
+                <div class="intro-bottom-top-text">Переход от понятия «рекламный бюджет»</div>
+                <div class="intro-bottom-bottom-text">К оптимизации стоимости каждого этапа «воронки продаж»</div>
+            </div>
+            <div class="intro-bottom-col">
+                <div class="intro-bottom-top-text">Переход от отчетности «На коленке»</div>
+                <div class="intro-bottom-bottom-text">К прозрачной картине бизнеса и автоматизации любого процесса клиники</div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="intro-bottom-button-wrap">
+    <div class="mbox">
+        <div class="button-calc">
+            <div class="aside-sub-button">
+                <div class="aside-sub-item">
+                    <div class="aside-sub-text"><span>Для более полного ознакомления загрузите нашу презентацию:</span></div>
+                    <div class="aside-sub-button">
+                        <a href="#"><span>Загрузить  нашу презентацию</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
